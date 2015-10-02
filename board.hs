62e3fd72d7d1dd5242e0a63569f8c2c049f2ced2
nayakrahul <rahulnk521@gmail.com>
2015-10-03 00:12:46 +0530

    move rules added

--- board.hs
+++ board.hs
@@ -2,8 +2,9 @@ import Data.Foldable
 import Data.Sequence
 
 type Board = [[Maybe Piece]]
-
-data Piece = Piece{color::Color,player::Player} 
+--type Status = Maybe Piece
+data Piece = Piece{color::Color,player::Player}
+data Status = Maybe Piece
 data Color = White | Black 
 data Player = King | Queen | Rook | Knight | Bishop | Pawn 
 
@@ -52,6 +53,7 @@ br = Piece{color=Black,player=Rook}
 bn = Piece{color=Black,player=Knight}
 bp = Piece{color=Black,player=Pawn}
 
+
 initialBoard :: Board
 initialBoard = [[Just wr,Just wn,Just wb,Just wq,Just wk,Just wb,Just wn, Just wr],
 				[Just wp,Just wp,Just wp,Just wp,Just wp,Just wp,Just wp, Just wp],
@@ -63,8 +65,8 @@ initialBoard = [[Just wr,Just wn,Just wb,Just wq,Just wk,Just wb,Just wn, Just w
 				[Just br,Just bn,Just bb,Just bq,Just bk,Just bb,Just bn, Just br]]
 
 
-change::Int -> Int -> Int -> Int -> Board -> Board
-change x1 y1 x2 y2 a = do 
+changeBoard::Int -> Int -> Int -> Int -> Board -> Board
+changeBoard x1 y1 x2 y2 a = do 
 		let p = (a!!x1)!!y1
 		let b2 | x1 == x2  = toList $ update y1 Nothing $ fromList (toList $ update y2 p $ fromList (a!!x2))
 			   | otherwise = toList $ update y2 p $ fromList (a!!x2)
@@ -80,32 +82,44 @@ move x b chance = do
 	 	let a2 = (read (x!!1)::Int)
 	 	let a3 = (read (x!!2)::Int)
 	 	let a4 = (read (x!!3)::Int)
-	 	let a5 = a1 >= 0 && a1 <= 7 && a2 >= 0 && a2 <= 7 && a3 >= 0 && a3 <= 7 && a4 >= 0 && a4 <= 7
-	 	if a5
+	 	let inRange = a1 >= 0 && a1 <= 7 && a2 >= 0 && a2 <= 7 && a3 >= 0 && a3 <= 7 && a4 >= 0 && a4 <= 7
+	 
+	 	if inRange && (a1,a2) /= (a3,a4) 
 	 		then do
-			 	let col = color $ (\(Just x) -> x) ((b!!a1)!!a2)
-			 	if( ((chance == True && col == White) || (chance == False && col == Black)) && a5 )
-			 		then do
-			 			let board = change a1 a2 a3 a4 b
-			 			let board' = (map.map) convert board
-			 			let print' n | n == 7    = print ((board'!!n))
-			 					 	 | otherwise = do 
-			 			  						print ((board'!!n))
-			 			  						print' (n+1)
-			 			print' 0
-			 			c <- getLine
-			 			let x = (words $ c)
-			 			move x board (not chance)
-			 		else do 
-			 			putStrLn "Invalid Move - Play again"
-			 			c <- getLine
-			 			let x = (words $ c)
-			 			move x b (chance)
-	 	else do
-	 		putStrLn "Invalid Input - Play again"
-			c <- getLine
-			let x = (words $ c)
-			move x b (chance)
+	 			let initial_empty = (convert ((b!!a1)!!a2)) == "  "
+	 			if (not initial_empty) 
+	 				then do
+	 					let col1 = color $ (\(Just x) -> x) ((b!!a1)!!a2)
+	 					let final_empty = (convert ((b!!a3)!!a4)) == "  "
+	 					let col2 | final_empty = if col1 == White then Black else White
+	 						 	 | otherwise   = color $ (\(Just x) -> x) ((b!!a3)!!a4)
+					 	if ((chance == True && col1 == White) || (chance == False && col1 == Black)) && col1 /= col2
+					 		then do
+					 			let board = changeBoard a1 a2 a3 a4 b
+					 			let board' = (map.map) convert board
+					 			let print' n | n == 7    = print ((board'!!n))
+					 					 	 | otherwise = do 
+					 			  						print ((board'!!n))
+					 			  						print' (n+1)
+					 			print' 0
+					 			c <- getLine
+					 			let x = (words $ c)
+					 			move x board (not chance)
+					 		else do 
+					 			putStrLn "Invalid Input - Play again"
+					 			c <- getLine
+					 			let x = (words $ c)
+					 			move x b (chance)
+	 				else do 
+	 					putStrLn "Invalid Input - Play again"
+						c <- getLine
+						let x = (words $ c)
+						move x b (chance) 
+	 		else do
+		 		putStrLn "Invalid Input - Play again"
+				c <- getLine
+				let x = (words $ c)
+				move x b (chance)
 
 convert::Maybe Piece -> String 
 convert (Just x) = show x 
@@ -126,28 +140,39 @@ main = do
 	 let a2 = (read (x!!1)::Int)
 	 let a3 = (read (x!!2)::Int)
 	 let a4 = (read (x!!3)::Int)
-	 let a5 = a1 >= 0 && a1 <= 7 && a2 >= 0 && a2 <= 7 && a3 >= 0 && a3 <= 7 && a4 >= 0 && a4 <= 7
-	 if a5
-	 	then do
-	 		let col = color $ (\(Just x) -> x) ((initialBoard!!a1)!!a2)
+	 let inRange = a1 >= 0 && a1 <= 7 && a2 >= 0 && a2 <= 7 && a3 >= 0 && a3 <= 7 && a4 >= 0 && a4 <= 7
 	 
-	 		if( chance == True && col == White && a5 )
-	 			then do 
-	 				let board = change a1 a2 a3 a4 initialBoard
-			 		let board' = (map.map) convert board
-			 		let print' n | n == 7    = print ((board'!!n))
-			 					 | otherwise = do 
-			 			  						print ((board'!!n))
-			 			  						print' (n+1)
-			 		print' 0
-			 		c <- getLine
-			 		let x = (words $ c)
-			 		move x board (not chance)
-			 	else do
-			 		putStrLn "Invalid Move - White to play - Play again"
-			 		c <- getLine
-			 		let x = (words $ c)
-			 		move x initialBoard (chance)
+	 if inRange && (a1,a2) /= (a3,a4) 
+	 	then do
+	 		let initial_empty = (convert ((initialBoard!!a1)!!a2)) == "  "
+	 		if (not initial_empty) 
+	 			then do
+	 				let col1 = color $ (\(Just x) -> x) ((initialBoard!!a1)!!a2)
+	 				let final_empty = (convert ((initialBoard!!a3)!!a4)) == "  "
+	 				let col2 | final_empty = Black
+	 						 | otherwise   = color $ (\(Just x) -> x) ((initialBoard!!a3)!!a4)
+			 		if( chance == True && col1 == White && col2 == Black)
+			 			then do 
+			 				let board = changeBoard a1 a2 a3 a4 initialBoard
+					 		let board' = (map.map) convert board
+					 		let print' n | n == 7    = print ((board'!!n))
+					 					 | otherwise = do 
+					 			  						print ((board'!!n))
+					 			  						print' (n+1)
+					 		print' 0
+					 		c <- getLine
+					 		let x = (words $ c)
+					 		move x board (not chance)
+					 	else do
+					 		putStrLn "Invalid Input - Play again"
+					 		c <- getLine
+					 		let x = (words $ c)
+					 		move x initialBoard (chance)
+	 			else do 
+	 				putStrLn "Invalid Input - Play again"
+					c <- getLine
+					let x = (words $ c)
+					move x initialBoard (chance) 
 		else do
 			putStrLn "Invalid Input - Play again"
 			c <- getLine
